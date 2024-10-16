d23034734688c34ad4138d6aa5c9e9be949f710a
Gabriel Cobabe <gabe.cobabe@malad.us>
2024-10-16 05:58:37 +0000

    It works now. Very nice.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,18 @@
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        JFrame frame = new JFrame("HELLO WORLD");
+        JPanel panel = new JPanel();
+        panel.setPreferredSize(new Dimension(500,500));
+        frame.add(panel);
+        frame.pack();
+        frame.setVisible(true);
+
     }
 }
